a0a2f847c00d5909e07b9825d60ebd274bba0a32
ptitbob <github@shipstone.org>
2016-03-18 00:33:48 +0100

    Initialization reading configuration from annotation

--- src/main/java/org/shipstone/swagger/integration/core/configuration/reader/AnnotationConfigurationReader.java
+++ src/main/java/org/shipstone/swagger/integration/core/configuration/reader/AnnotationConfigurationReader.java
@@ -1,8 +1,10 @@
 package org.shipstone.swagger.integration.core.configuration.reader;
 
+import org.shipstone.swagger.integration.annotation.SwaggerUIConfiguration;
 import org.shipstone.swagger.integration.core.configuration.Configuration;
 
 import javax.servlet.ServletContext;
+import javax.ws.rs.ApplicationPath;
 import java.lang.annotation.Annotation;
 import java.util.Map;
 
@@ -13,8 +15,11 @@ public class AnnotationConfigurationReader extends ConfigurationReader {
 
   @Override
   protected Configuration readConfigurationFrom(Configuration configuration, ServletContext servletContext) {
-    Map<Class<? extends Annotation>, String> annotatedClassMap = new ByteCodeAnnotationScanner(servletContext).get();
-    return null;
+    Map<Class<? extends Annotation>, String> annotatedClassMap = new ByteCodeAnnotationScanner(servletContext, SwaggerUIConfiguration.class, ApplicationPath.class).get();
+    if (annotatedClassMap.size() > 0) {
+
+    }
+    return configuration;
   }
 
 }
