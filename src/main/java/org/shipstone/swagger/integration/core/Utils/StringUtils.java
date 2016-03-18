6ebc00eb47f24b502fce6e28f8f90600a4a53fe3
ptitbob <github@shipstone.org>
2016-03-19 00:06:29 +0100

    Add utils static class

--- src/main/java/org/shipstone/swagger/integration/core/Utils/StringUtils.java
+++ src/main/java/org/shipstone/swagger/integration/core/Utils/StringUtils.java
@@ -1,7 +1,20 @@
-package org.shipstone.swagger.integration.core.Utils;
+package org.shipstone.swagger.integration.core.utils;
+
+import static org.shipstone.swagger.integration.core.configuration.DefaultConfigurationProvider.EMPTY;
 
 /**
  * @author francois
  */
 public class StringUtils {
+
+
+  public static boolean isNotEmpty(String string) {
+    return !isEmpty(string);
+  }
+
+  public static boolean isEmpty(String string) {
+    return string == null || EMPTY.equals(string.trim());
+  }
+
+
 }
