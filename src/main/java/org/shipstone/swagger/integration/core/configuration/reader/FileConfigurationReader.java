1192959fc14eb7a529b80308a06c45b0dd3cf835
ptitbob <github@shipstone.org>
2016-03-21 20:17:55 +0100

    Clean imports

--- src/main/java/org/shipstone/swagger/integration/core/configuration/reader/FileConfigurationReader.java
+++ src/main/java/org/shipstone/swagger/integration/core/configuration/reader/FileConfigurationReader.java
@@ -5,7 +5,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.servlet.ServletContext;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
